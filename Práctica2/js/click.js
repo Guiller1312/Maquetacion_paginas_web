ff261ee0b27e43c22281b2a247cbac51f4369d11
Guiller1312 <guillermodf76@gmail.com>
2025-02-04 18:17:34 +0100

    Práctica 2 JS añadido y enlace al html

---
+++ Práctica2/js/click.js
@@ -0,0 +1,6 @@
+function press() {
+
+    const cuadro_pequeño = document.getElementById("grid_principal");
+    cuadro_pequeño.style.padding = "10%";
+    cuadro_pequeño.style.backgroundColor = "gray";
+}

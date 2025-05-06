fa2a05b6dcc48dc725b520e07559c12d7a9db266
Guiller1312 <guillermodf76@gmail.com>
2025-05-06 15:58:10 +0200

    Ordenar ficheros y nuevos ejercicios

--- COMPONENTES/swiperComplejo/js/jsButtons.js
+++
@@ -1,9 +0,0 @@
-const swiperEl = document.querySelector('swiper-container');
-const buttonEl = document.querySelector('#but1');
-const buttonE2 = document.querySelector('#but2');
-buttonEl.addEventListener('click', () => {
-	swiperEl.swiper.slideNext();
-});
-buttonE2.addEventListener('click', () => {
-	swiperEl.swiper.slidePrev();
-});

dae34483f1d78f1181a249d2ca515417e3e1aa06
Guiller1312 <guillermodf76@gmail.com>
2025-05-08 17:46:44 +0200

    Ejercicios clase

--- Practicas_JS/Practica_visualizar por consola/JS/Error_/19_NBinario.js
+++ Practicas_JS/Practica_visualizar por consola/JS/Error_/19_NBinario.js
@@ -1,10 +1,13 @@
 let num = 10;
 let num2;
 for (let i = 0; i < num; i++) {
-    if (num % 2 == 0 && num != 2){
-        num2 = 1;
-        i++;
-    }else{
-        num2 = 0;
+    if (num > 1) {
+        if (num % 2 == 0 && num != 2) {
+            num2 = 1;
+
+        } else {
+            num2 = 0;
+        }
     }
 }
+console.error(num2);

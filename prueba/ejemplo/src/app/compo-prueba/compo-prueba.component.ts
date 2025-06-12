16d8a33788b578f6db3b6ad864a9c4e3938df920
Guiller1312 <guillermodf76@gmail.com>
2025-06-12 15:32:51 +0200

    Ejemplo clase angular

---
+++ prueba/ejemplo/src/app/compo-prueba/compo-prueba.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compo-prueba',
+  imports: [],
+  templateUrl: './compo-prueba.component.html',
+  styleUrl: './compo-prueba.component.css'
+})
+export class CompoPruebaComponent {
+
+}
